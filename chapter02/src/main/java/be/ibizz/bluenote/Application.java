653e75b0b5d441f173e09f936d197704ab6363fc
andyvdh <andy.vandenheuvel@gmail.com>
2015-01-05 20:27:41 +0100

    chapter 02

---
+++ chapter02/src/main/java/be/ibizz/bluenote/Application.java
@@ -0,0 +1,25 @@
+package be.ibizz.bluenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String index() {
+        return "<h1>Hello Bluemix (via Spring boot)</h1>";
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
